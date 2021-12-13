11476f2bf72ae58d42dc4f3baa4b2af1ef0b6387
Andrew Leacock <andrewleacock96@gmail.com>
2021-12-13 11:29:18 -0500

    dynamic background

---
+++ time.js
@@ -0,0 +1,14 @@
+
+if (new Date().getHours() < 12){
+    document.body.style.background = "url('https://64.media.tumblr.com/5d8132706dcd2adbf4957093b37fc267/tumblr_pwe0mm0vhe1uxrf48o1_640.gifv')";
+    document.body.style.backgroundSize = "cover"
+
+}
+
+
+if (new Date().getHours() > 20 ){
+    document.body.style.background = "url('https://64.media.tumblr.com/2bc084c044357fe1e782dd67441c9d4b/9bf1ca157771228f-d8/s1280x1920/7e06cf46ca6bb67b80f9be339715be6ea54052c4.gifv')";
+    document.body.style.backgroundSize = "cover"
+
+}
+
